24bb8033f5e0b3e709e1ba275f8c084f1f2d6a7d
mdicroce <50848313+mdicroce@users.noreply.github.com>
2021-07-26 23:45:33 -0300

    no se si se cambio algo

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-import 'fontsource-roboto'
 //routes
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
